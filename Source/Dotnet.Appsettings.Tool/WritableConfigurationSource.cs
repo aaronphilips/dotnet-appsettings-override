5d9acf89b55135edb3ed5a30c1c4a9fc52c06872
Aaron Philips <aphilips@ualberta.ca>
2022-04-29 13:23:27 -0600

    Initial Commit

---
+++ Source/Dotnet.Appsettings.Tool/WritableConfigurationSource.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Dotnet.Appsettings.Tool;
+
+public class WritableConfigurationSource : JsonConfigurationSource
+{
+    public WritableConfigurationSource(string? appsettingsJson)
+    {
+        Path = appsettingsJson;
+    }
+
+    public override IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        EnsureDefaults(builder);
+        return new WritableJsonConfigurationProvider(this);
+    }
+}
---
+++ Source/Dotnet.Appsettings.Tool/WritableJsonConfigurationProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Dotnet.Appsettings.Tool;
+
+public class WritableJsonConfigurationProvider : JsonConfigurationProvider
+{
+    public WritableJsonConfigurationProvider(JsonConfigurationSource source) : base(source)
+    {
+    }
+
+    public override void Set(string key, string value)
+    {
+        base.Set(key, value);
+        var physicalPath = Source.FileProvider.GetFileInfo(Source.Path).PhysicalPath;
+        var file = File.ReadAllText(physicalPath);
+        var config = JsonSerializer.Deserialize<JsonNode>(file) ?? throw new Exception();
+        SetValueRecursively(key, config, value);
+
+        File.WriteAllText(physicalPath, JsonSerializer.Serialize(config, options: new JsonSerializerOptions
+        {
+            WriteIndented = true,
+        }));
+    }
+
+    private static void SetValueRecursively<T>(string sectionPathKey, JsonNode jsonObj, T value)
+    {
+        var remainingSections = sectionPathKey.Split(":", 2);
+        var currentSection = remainingSections[0];
+        if (remainingSections.Length > 1)
+        {
+            var nextSection = remainingSections[1];
+            var obj = jsonObj[currentSection]!;
+            SetValueRecursively(nextSection, obj, value);
+        }
+        else
+        {
+            jsonObj[sectionPathKey] = value!.ToString();
+        }
+    }
+}
